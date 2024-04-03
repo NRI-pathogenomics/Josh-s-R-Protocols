0c0252f0cb56fc67adb78ea20a93820fe81a1498
j-j-h62 <jjh62@kent.ac.uk>
2024-04-03 15:12:03 +0100

    Added both the individual and averaged AUDPC calculation elements from the previous two scripts. At the moment the old, seperate plot code using ggplot2 to generate a single plot per averaged value for the different genotypes is used currently but it will be rewritten to have the mock and the innoculated values on the same plots (1x Mock and 1x Innoculated plot)

--- Combined Average Plots/Combined Average plots.R
+++ Combined Average Plots/Combined Average plots.R
@@ -35,6 +35,7 @@ Tail_Value <- Path_Assay$block[length(Path_Assay$block)]
 
 setNames(Path_Assay, nm=c("Genotype", "block",	"treatment.", "number", "Day 28", "Day 35", "Day 49", "Day 56"))
 
+## Individual AUDPCs
 Result_Genotype <- list()
 Result_Treatment <- list()
 Result_AUDPC <-list()
@@ -83,28 +84,103 @@ for(i in 1:Tail_Value){
   }
   
 }
-#Generate the Results and reform the original
+#Generate the Results and reform the original data frame
 Results <- cbind(Result_Genotype,Result_Type,Result_Treatment, Result_AUDPC)
 Indv_Mock_Results <- subset(Results, Result_Type=="M")
 Indv_Inno_Results <- subset(Results, Result_Type=="I")
 
-# FORGET THE AVERAGE VALUES AND PLOTTING THEM FOR NOW KEEP THE PROGRAMS SEPERATE! (COME BACK AT A LATER )
+## Calculate the Mean values
+# reuse Mock_sub and Inno_sub
+Mock_means <- data.frame()
+Mock_AUDPC <- list()
+Inno_means <- data.frame()
+Inno_AUDPC <- list()
+## check if mock output files exist and create them if not in the Onedrive local repository
+if(file.exists("/Users/joshhoti/Library/CloudStorage/
+               OneDrive-UniversityofKent/Postgraduate/GitHub-Josh-s-R-Protocols/Combined Average plots/Mock Averaged Results")==FALSE){
+  file.create("Mock Averaged Results")
+}
+
+if(file.exists("/Users/joshhoti/Library/CloudStorage/
+               OneDrive-UniversityofKent/Postgraduate/GitHub-Josh-s-R-Protocols/Combined Average plots/Innoculated Averaged Results")==FALSE){
+  file.create("Innoculated Averaged Results")
+}
+## Calculate and save the Averaged AUPDC results
+Genotypes <- c(unique(Path_Assay$Genotype))
+GenoNumber <- length(Genotypes)
+for(i in 1:GenoNumber){
+  Mockotype <- subset(Mock_Sub, Genotype==Genotypes[i])
+  #Calculate audpc using agricolae on the subset then leave room for the plots, same for innoculated
+  print(paste0("Averages for ", Genotypes[i]))
+  dates <- c(28,35,49,56)
+  means <- colMeans(Mockotype[5:8])
+  print(means)
+  Mock_means <- append(Mock_means, cbind(Genotypes[i], means))
+  ##get it to add the means as it goes to the means dataframe
+  AUDPC <- audpc(means, dates, type="absolute")
+  print(paste0("AUDPC for ",Genotypes[i], " is: ", AUDPC))
+  Mock_AUDPC <-append(Mock_AUDPC,AUDPC)
+  
+  #PLOT AS THE LOOP RUNS - TEMPORARY
+  plot_data <- as.data.frame(cbind(dates, means))
+  plot <- ggplot(data=plot_data, mapping=aes(x=dates, y=means, )) + geom_line() + geom_point() + 
+    annotate("text", x = 35, y = 3.5, label = paste0("AUDPC: ", as.character(AUDPC))) +
+    geom_ribbon(aes(ymin = min(means), ymax = means), fill = "#1b98e0")
+  plot <- plot + labs(title = paste0(Genotypes[i], " Mock Innoculation Average Disease Progression"))
+  show(plot)
+}
+
+
+# Innoculated means
+for(i in 1:GenoNumber){
+  Innotype <- subset(Inno_Sub, Genotype==Genotypes[i])
+  #Calculate audpc using agricolae on the subset then leave room for the plots, same for innoculated
+  print(paste0("Averages for ", Genotypes[i]))
+  dates <- c(28,35,49,56)
+  means <- colMeans(Innotype[5:8])
+  print(means)
+  AUDPC <- audpc(means, dates, type="absolute")
+  print(paste0("AUDPC for ",Genotypes[i], " is: ", AUDPC))
+  Inno_AUDPC <-append(Inno_AUDPC,AUDPC)
+  #PLOT AS THE LOOP RUNS - TEMPORARY
+  plot_data <- as.data.frame(cbind(dates, means))
+  plot <- ggplot(data=plot_data, mapping=aes(x=dates, y=means, )) + geom_line() + geom_point() + 
+    annotate("text", x = 35, y = 3.5, label = paste0("AUDPC: ", as.character(AUDPC))) +
+    geom_ribbon(aes(ymin = min(means), ymax = means), fill = "#1b98e0")
+  plot <- plot + labs(title = paste0(Genotypes[i], " Verticilium Innoculation Average Disease Progression"))
+  show(plot)
+}
+#Create results data frames
+Mock_results <- cbind(Genotypes, Mock_AUDPC)
+Inno_results <- cbind(Genotypes, Inno_AUDPC)
+#Save Results
+Save_File_Mock <- "/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/GitHub-Josh-s-R-Protocols/Combined Average Plots/MockAUDPC.csv"
+Save_File_Inno <- "/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/GitHub-Josh-s-R-Protocols/Combined Average Plots/InnoAUDPC.csv"
+if(file.exists(Save_File_Mock) == TRUE){
+  file.remove(Save_File_Mock)
+}
+if(file.exists(Save_File_Inno) == TRUE){
+  file.remove(Save_File_Inno)
+}
+write.csv(x = Mock_results, file = Save_File_Mock )
+write.csv(x = Inno_results, file = Save_File_Inno)
+
 ##Plot Data Here
 #Insert AUDPC plot for Mock and Innoculated here
 
-##Mock AUDPC plot
-# Reshape the data from wide to long format - It is NOT Going to work plotting the individual data on the same plot like this 
-# subset the different genotypes and plot them on the same plots but keep the genotypes seperate or go back to using averaged values and plot them on the same plots
-Mock_long <- tidyr::pivot_longer(Mock_Sub, cols = starts_with("Day"), names_to = "Day", values_to = "Disease_Score")
-
-# Convert Day column to numeric
-Mock_long$Day <- as.numeric(gsub("Day ", "", Mock_long$Day))
-
-# Create plot using ggplot2
-ggplot(Mock_long, aes(x = Day, y = Disease_Score, group = interaction(Genotype, block, treatment., number), color = interaction(Genotype, block, treatment., number))) +
-  geom_line() +
-  labs(x = "Day", y = "Disease Score", color = "Group") +
-  theme_minimal()
+# # Mock AUDPC plot
+# # Reshape the data from wide to long format - It is NOT Going to work plotting the individual data on the same plot like this 
+# # subset the different genotypes and plot them on the same plots but keep the genotypes seperate or go back to using averaged values and plot them on the same plots
+# Mock_long <- tidyr::pivot_longer(Mock_Sub, cols = starts_with("Day"), names_to = "Day", values_to = "Disease_Score")
+# 
+# # Convert Day column to numeric
+# Mock_long$Day <- as.numeric(gsub("Day ", "", Mock_long$Day))
+# 
+# # Create plot using ggplot2
+# ggplot(Mock_long, aes(x = Day, y = Disease_Score, group = interaction(Genotype, block, treatment., number), color = interaction(Genotype, block, treatment., number))) +
+#   geom_line() +
+#   labs(x = "Day", y = "Disease Score", color = "Group") +
+#   theme_minimal()
 
 ##Inno AUDPC plot
 
