41043b772186c32b6616561f0ca16732cf8c0d0f
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-07 11:49:00 +0100

    Set up a Master Script to run the Path Assay Analysis one after the other - this is V1.

---
+++ Master Script/Master Script.R
@@ -0,0 +1,10 @@
+# Master Script
+# AUDPC
+source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Combined Average Plots/Combined Average plots.R")
+
+#Shapiro tests
+
+source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Shapiro Tests/Shapiro Wilks Test.R")
+
+# ANOVA tests
+source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/ANOVA/Anova example.R")
