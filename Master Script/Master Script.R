a1391c3fbce03c0a3bff4752c2e4e2faebf40008
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-08 15:06:40 +0100

    Updated the Master Script to include the residuals test after the Shapiro test

--- Master Script/Master Script.R
+++ Master Script/Master Script.R
@@ -6,5 +6,10 @@ source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Pro
 
 source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Shapiro Tests/Shapiro Wilks Test.R")
 
+# Residuals test
+
+source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Shapiro Tests/Residuals Test.R")
+
 # ANOVA tests
 source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/ANOVA/Anova example.R")
+
