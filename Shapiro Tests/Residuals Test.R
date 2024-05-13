ccc14d563872770e5818b35d53c76ccca969bf59
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-13 16:14:46 +0100

    Added Log10 transformation to Residuals Test And attempted to transform the data.

--- Shapiro Tests/Residuals Test.R
+++ Shapiro Tests/Residuals Test.R
@@ -5,3 +5,38 @@ shapiro.test(x)
 m <- mean(x)
 s <- sd(x)
 ks.test(x,"pnorm",m,s)
+
+# Transform Data if the residuals are not normally distributed based on the boxcox(anv.model) output from the ANOVA test
+
+AUDPC_Values <- as.numeric(Results$Result_AUDPC)
+log_AUDPC_Values <- log(AUDPC_Values)
+
+# Add Log values to anv.data
+
+anv.data$Result_AUDPC_Log10 <- log_AUDPC_Values
+
+# Transform Data if the residuals are not normally distributed based on the boxcox(anv.model) output from the ANOVA test
+# inverse sqroot
+AUDPC_Values <- as.numeric(Results$Result_AUDPC)
+Invsq_AUDPC_Values <- 1/AUDPC_Values^2
+
+# Add Log values to anv.data
+
+anv.data$Result_Invsq_AUDPC <- Invsq_AUDPC_Values
+
+#Residuals Test on Log values
+
+x<-resid(lm(Result_Invsq_AUDPC~ Result_Type * Result_Genotype, data=anv.data))
+hist(x, main = "Residual Distribution of Regression Model for log10(AUDPC) by Type and Genotype")
+shapiro.test(x)
+m <- mean(x)
+s <- sd(x)
+ks.test(x,"pnorm",m,s)
+
+
+# redo shapiro.wilkes
+inv.sq.anv.model <- aov(Invsq_AUDPC_Values ~ Result_Type * Result_Genotype, data = anv.data)
+shapiro.test(anv.data$Result_Invsq_AUDPC)
+#still don't have normally-distributed residuals
+#check residuals
+hist()
