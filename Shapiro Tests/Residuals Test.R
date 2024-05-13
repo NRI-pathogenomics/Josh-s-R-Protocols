fdf4b5eec4a75bdda4163f2cf2f7d0d07d0d79bb
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-13 15:31:37 +0100

    Updated the hist() function to have the title: "Residual Distribution of Regression Model for AUDPC by Type and Genotype"

--- Shapiro Tests/Residuals Test.R
+++ Shapiro Tests/Residuals Test.R
@@ -1,6 +1,6 @@
 # Residuals Test
 x<-resid(lm(Result_AUDPC ~ Result_Type * Result_Genotype, data=anv.data))
-hist(x)
+hist(x, main = "Residual Distribution of Regression Model for AUDPC by Type and Genotype")
 shapiro.test(x)
 m <- mean(x)
 s <- sd(x)
