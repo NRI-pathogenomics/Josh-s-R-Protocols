0d484ac5441a3d4173ac430f997170c8e381c7d8
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-13 15:08:53 +0100

    working out how to properly format the y, x and v parameters for reml

--- REML Test/REML Test.R
+++ REML Test/REML Test.R
@@ -24,5 +24,33 @@ if(exists("Indv_Mock_Results") == FALSE){
 if(exists("Indv_Mock_Results") == FALSE){
   Indv_Inno_Results <- subset(Results, Result_Type == "I")
 }
+# mock REML
+# define the y, x and v values
 
-reml(y, v, x, data, RE.constraints = NULL, RE.startvalues = 0.1)
+y <- as.numeric(Indv_Mock_Results$Result_AUDPC)
+
+v <- rep(var(y), nrow(Indv_Mock_Results))
+
+Result_Genotype <- unlist(Indv_Mock_Results$Result_Genotype)
+Result_Type <- unlist(Indv_Mock_Results$Result_Type)
+
+# Create x matrix
+x <- cbind(Result_Genotype, Result_Type)
+
+reml(y, v, x, data=Indv_Mock_Results, RE.constraints = NULL, RE.startvalues = 0.1)
+
+# inno REML
+# define the y, x and v values
+
+y <- as.numeric(Indv_Inno_Results$Result_AUDPC)
+
+v <- rep(var(y), nrow(Indv_Inno_Results))
+
+Result_Genotype <- unlist(Indv_Inno_Results$Result_Genotype)
+Result_Type <- unlist(Indv_Inno_Results$Result_Type)
+
+# Create x matrix
+x <- cbind(Result_Genotype, Result_Type)
+
+reml(y, v, x, data=Indv_Inno_Results, RE.constraints = NULL, RE.startvalues = 0.1)
+
