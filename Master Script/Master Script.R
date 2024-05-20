b964cc0bd27b5cf9f17b28f97c18571207ce97e7
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-20 10:35:42 +0100

    Updated the master script to contain a few options for the user.

--- Master Script/Master Script.R
+++ Master Script/Master Script.R
@@ -4,13 +4,25 @@ source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Pro
 
 #Shapiro tests
 
-source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Shapiro Tests/Shapiro Wilks Test.R")
+#source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Shapiro Tests/Shapiro Wilks Test.R")
 
 # ANOVA tests
 source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/ANOVA/Anova example.R")
 
+question_1 <- readline("Do you want to try transform the data?")
+
+if(question_1 == "Y"){
+  source("/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/ANOVA/ANOVA Data Transformations.R")
+}
+if(question_1 == "N"){
+  # REML test
+  
+  source("/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/REML Test.R") 
+}
+
+
+
 # Residuals test
 
 source("~/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Shapiro Tests/Residuals Test.R")
-
 
