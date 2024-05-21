3ec366d264ddfdf3d81412d9b8ca069eceb3c53f
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-21 13:41:34 +0100

    Added the same loop from the ANOVA script the data is still transformed in one go but it's subset in the loop.

--- ANOVA/ANOVA With Transformation Individual Genotypes.R
+++ ANOVA/ANOVA With Transformation Individual Genotypes.R
@@ -78,35 +78,85 @@ transformed_AUDPC <- bestNormalize(anv.data$Result_AUDPC)
 
 transformed_data <- transformed_AUDPC$x.t
 
-par(mfrow=c(2,1))
-hist(transformed_data)
-qqnorm(transformed_data)
-qqline(transformed_data)
-shapiro.test(transformed_data)
-
-
-# #redo ANOVA test by selecting a set of selected data in EX1
-# anv.mod<-aov(Ex_all[["1"]] ~ Result_Type * Result_Genotype, data = anv.data)
-# Run anv.mod for the transformed data
-anv.mod <- aov(transformed_data ~ Result_Type * Result_Genotype, data = anv.data)
-print(anv.mod)
-
-
-# examine differences between specific pairs of treatments, we can use a post-hoc test, 
-#e.g., Tukeys Honest Significant Differences
-print(posthoc<-TukeyHSD(anv.mod,"Result_Type"))
-
-#Test the Residuals again - this is the real test of whether the transformation
-
 Transformed_Results <- cbind(Results, transformed_data)
 
 Transformed_Results$Result_AUDPC <- as.numeric(Transformed_Results$Result_AUDPC)
 Transformed_Results$Result_Type <- as.character(Transformed_Results$Result_Type)
 Transformed_Results$Result_Genotype <- as.character(Transformed_Results$Result_Genotype)
 
-x<-resid(lm(Result_AUDPC ~ Result_Type * Result_Genotype, data=Transformed_Results))
-hist(x, main = "Residual Distribution of BestNormalized results for AUDPC by Type and Genotype")
-shapiro.test(x)
-m <- mean(x)
-s <- sd(x)
-ks.test(x,"pnorm",m,s)
+# #redo ANOVA test by selecting a set of selected data in EX1
+# anv.mod<-aov(Ex_all[["1"]] ~ Result_Type * Result_Genotype, data = anv.data)
+# Run anv.mod for the transformed data
+
+for(i in 1:GenoNumber){
+  print(paste("Running Results for Genotype: ", Genotypes[i]))
+  Results_Sub <- subset(Transformed_Results, Result_Genotype == Genotypes[i])
+  print(anv.data<-Results_Sub)
+  attach(anv.data)
+  
+  anv.data$Result_AUDPC <- as.numeric(anv.data$Result_AUDPC)
+  anv.data$Result_Genotype <- as.character(anv.data$Result_Genotype)
+  anv.data$Result_Type <- as.character(anv.data$Result_Type)
+  # Create a variable for boxcox
+  # Run aov
+  anv.model <- aov(transformed_data ~ Result_Type, data = anv.data)
+  
+  # here x would be disease score, y would be genotype z could be included as block or run without for a one way anova
+  
+  print(anv.model)
+  
+  # Check normality with hist, qqplots
+  par(mfrow=c(4,2))
+  hist(anv.data$Result_AUDPC, main = paste("Distribution of AUDPC values for ", Genotypes[i]))
+  qqnorm(anv.data$Result_AUDPC)
+  qqline(anv.data$Result_AUDPC)
+  # Check normalitly with shapiro-wilks - with a tryCatch statement to catch whenever the AUDPCs are all the same
+  shapiro_result <- tryCatch({
+    shapiro.test(anv.data$Result_AUDPC)
+  }, error = function(e) {
+    message("Error in Shapiro-Wilk test for Genotype ", Genotypes[i], ": ", e$message)
+    return(NULL)
+  })
+  
+  if (is.null(shapiro_result)) {
+    detach(anv.data)
+    next
+  }
+  
+  # If data isnt normal Run a Box-Cox proceedure to obtain optimal transformation
+  MASS::boxcox(anv.model)
+  # Produces a plot of likelihood of the parameter lambda against values of lambda
+  # from -2 to 2
+  # Dotted vertical lines indicate the ideal value of lambda
+  # Refine range of lambda eg from 0 to 0.5 in increments of 0.1
+  MASS::boxcox(anv.model, lambda = seq(0, 0.5, 0.1))
+  
+  # Plot boxcoxs
+  par(mfrow=c(2,1))
+  MASS::boxcox(anv.model)
+  MASS::boxcox(anv.model, lambda = seq(0, 0.5, 0.1))
+  
+  tryCatch({
+    x <- resid(lm(Result_AUDPC ~ Result_Type, data = anv.data))
+    
+    if (all(x == x[1])) {
+      stop("all 'x' values are identical")
+    }
+    
+    x<-resid(lm(transformed_data ~ Result_Type, data=anv.data))
+    hist(x, main = "Residual Distribution of BestNormalized results for AUDPC by Type and Genotype")
+    shapiro.test(x)
+    m <- mean(x)
+    s <- sd(x)
+    ks.test(x,"pnorm",m,s)
+  }, error = function(e) {
+    message("Error in residuals test for Genotype ", Genotypes[i], ": ", e$message)
+    detach(anv.data)
+    next
+  })
+  
+  detach(anv.data)
+  # examine differences between specific pairs of treatments, we can use a post-hoc test, 
+  #e.g., Tukeys Honest Significant Differences
+  print(posthoc<-TukeyHSD(anv.mod,"Result_Type"))
+}
