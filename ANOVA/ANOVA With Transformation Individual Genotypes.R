1dd805d8b485c41885ec2ea041cea8ebf7a37125
yourGitHubUsername <jjh62@kent.ac.uk>
2024-05-21 13:33:18 +0100

    Created a new transformation script for the indiviual genotypes.

---
+++ ANOVA/ANOVA With Transformation Individual Genotypes.R
@@ -0,0 +1,112 @@
+#ANOVA Data Transformations
+
+# Load Package MASS
+# attach package library(MASS)
+# Load and attach data
+all_packages <- installed.packages()
+
+if(("MASS" %in% all_packages)==FALSE){
+  install.packages("MASS")}
+if(("ggpmisc" %in% all_packages)==FALSE){
+  install.packages("ggpmisc")}
+if(("jmv" %in% all_packages)==FALSE){
+  install.packages("jmv")}
+if(("Rmisc" %in% all_packages)==FALSE){
+  install.packages("Rmisc")}
+if(("emmeans" %in% all_packages)==FALSE){
+  install.packages("emmeans")}
+if(("multcomp" %in% all_packages)==FALSE){
+  install.packages("multcomp")}
+if(("Hmisc" %in% all_packages)==FALSE){
+  install.packages("Hmisc")}
+if(("lattice" %in% all_packages)==FALSE){
+  install.packages("lattice")}
+if(("multcompView" %in% all_packages)==FALSE){
+  install.packages("multcompView")}
+if(("bestNormalize" %in% all_packages)==FALSE){
+  install.packages("bestNormalize")}
+library(MASS)
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(ggpmisc)
+library(jmv)
+library(Rmisc)
+library(emmeans) 
+library(multcomp)
+library(Hmisc)
+library(lattice)
+library(multcompView)
+library(agricolae)
+library(bestNormalize)
+# Load the package
+## Power Of - Transformations
+
+# Create empty lists to store models and transformed data
+lm_models <- list()
+Ex_values <- list()
+
+# # Loop through the powers from -2 to 2 going up by 0.1 each time
+# for (power in seq(-2, 2, by = 0.1)) {
+#   # Try to transform data with the current power
+#   tryCatch({
+#     # Add data to original data set
+#     lamEx <- cbind(anv.data, anv.data$Result_AUDPC^power)
+#     Ex <- anv.data$Result_AUDPC^power
+#     
+#     # Append lm model and transformed data to lists
+#     lm_models[[as.character(power)]] <- lm(Ex ~ Result_Type * Result_Genotype, data = lamEx)
+#     Ex_values[[as.character(power)]] <- Ex
+#     
+#     # Plot histogram
+#     hist(Ex, main = paste("Distribution of AUDPC Values^", power))
+#     qqnorm(Ex)
+#     qqline(Ex)
+#     
+#     # Check normality with Shapiro-Wilk test
+#     shapiro.test(Ex)
+#   }, error = function(e) {
+#     # If an error occurs (e.g., data is transformed by the power of 0), print a message
+#     cat("Error occurred for power", power, ": ", conditionMessage(e), "\n")
+#   })
+# }
+
+# BestNormalize transformations
+# Best Normalize
+
+transformed_AUDPC <- bestNormalize(anv.data$Result_AUDPC)
+
+transformed_data <- transformed_AUDPC$x.t
+
+par(mfrow=c(2,1))
+hist(transformed_data)
+qqnorm(transformed_data)
+qqline(transformed_data)
+shapiro.test(transformed_data)
+
+
+# #redo ANOVA test by selecting a set of selected data in EX1
+# anv.mod<-aov(Ex_all[["1"]] ~ Result_Type * Result_Genotype, data = anv.data)
+# Run anv.mod for the transformed data
+anv.mod <- aov(transformed_data ~ Result_Type * Result_Genotype, data = anv.data)
+print(anv.mod)
+
+
+# examine differences between specific pairs of treatments, we can use a post-hoc test, 
+#e.g., Tukeys Honest Significant Differences
+print(posthoc<-TukeyHSD(anv.mod,"Result_Type"))
+
+#Test the Residuals again - this is the real test of whether the transformation
+
+Transformed_Results <- cbind(Results, transformed_data)
+
+Transformed_Results$Result_AUDPC <- as.numeric(Transformed_Results$Result_AUDPC)
+Transformed_Results$Result_Type <- as.character(Transformed_Results$Result_Type)
+Transformed_Results$Result_Genotype <- as.character(Transformed_Results$Result_Genotype)
+
+x<-resid(lm(Result_AUDPC ~ Result_Type * Result_Genotype, data=Transformed_Results))
+hist(x, main = "Residual Distribution of BestNormalized results for AUDPC by Type and Genotype")
+shapiro.test(x)
+m <- mean(x)
+s <- sd(x)
+ks.test(x,"pnorm",m,s)
