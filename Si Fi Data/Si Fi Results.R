517942bbd0cb2059046e34109ee3d1e8758653d5
yourGitHubUsername <jjh62@kent.ac.uk>
2024-06-10 13:20:30 +0100

    Added the data file to the repository

--- Si Fi Data/Si Fi Results.R
+++ Si Fi Data/Si Fi Results.R
@@ -21,6 +21,6 @@ library(reshape2)
 library(tidyverse)
 library(magrittr)
 library(plotrix)
-Si_Fi <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Master's/Research/Bioinformatic work/Si Fi.csv", 
+Si_Fi <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Master's/Research/Bioinformatic work/Si Fi Results.csv", 
                   header = TRUE, sep = ",", quote = "\"",
                   dec = ".", fill = TRUE, comment.char = "")
