168195b552d59bb20464332102aefe1fc2ea569b
yourGitHubUsername <jjh62@kent.ac.uk>
2024-06-10 13:40:54 +0100

    Tried a dotplot but realised that the total number of hits and efficient number of hits are given as hits per chromosome - since the chromosome doesn't really matter for the sake of the plot I am going to reshape the data in excel and go back to doing a bar plot

--- Si Fi Data/Si Fi Results.R
+++ Si Fi Data/Si Fi Results.R
@@ -24,23 +24,5 @@ library(plotrix)
 Si_Fi <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R-Protocols/Si Fi Data/Si Fi results.csv", 
                   header = TRUE, sep = ",", quote = "\"",
                   dec = ".", fill = TRUE, comment.char = "")
-# Extract Genome comparison type ("V.dahliae JR2" or "Col-0")
-data <- data %>%
-  mutate(Comparison = ifelse(grepl("V.dahliae JR2", Genome), "V.dahliae JR2", "Col-0"))
 
-# Ensure efficient hits are numeric and replace NA with 0
-data$Efficient.siRNA.hits[is.na(data$Efficient.siRNA.hits)] <- 0
-
-# Convert Genome to a factor to keep the order
-data$Genome <- factor(data$Genome, levels = unique(data$Genome))
-
-# Plot the bar chart
-ggplot(data, aes(x = Genome, fill = Comparison)) +
-  geom_bar(aes(y = Total.siRNA.hits), stat = "identity", position = "dodge") +
-  geom_bar(aes(y = Efficient.siRNA.hits), stat = "identity", position = "dodge", color = "black", alpha = 0.5) +
-  scale_fill_manual(values = c("V.dahliae JR2" = "blue", "Col-0" = "green")) +
-  labs(title = "Total vs Efficient siRNA Hits per Genome",
-       x = "Genome",
-       y = "Number of siRNA Hits",
-       fill = "Comparison") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+# Plot the dot plot
