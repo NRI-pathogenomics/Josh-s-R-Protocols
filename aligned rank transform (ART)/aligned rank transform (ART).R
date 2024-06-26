db6f7e50ed9ea90101c71c04641e9ad583e3573a
yourGitHubUsername <jjh62@kent.ac.uk>
2024-06-26 14:42:15 +0100

    Fixed the code so that it can process the results dataframe and use the correct base vs package functions so that everyhting works as it did on the example data. I also added a bar plot function which plots the cld results, showing the grouping of the results

--- aligned rank transform (ART)/aligned rank transform (ART).R
+++ aligned rank transform (ART)/aligned rank transform (ART).R
@@ -1,9 +1,12 @@
 # aligned rank transform (ART)
 ### Step 1: Install and Load Necessary Packages
-
-install.packages("ARTool")
-install.packages("emmeans")
-install.packages("multcompView") # for cld function
+if(("ARTool" %in% all_packages)==FALSE){
+  install.packages("ARTool")}
+if(("emmeans" %in% all_packages)==FALSE){
+  install.packages("emmeans")}
+if(("multcompView" %in% all_packages)==FALSE){
+  install.packages("multcompView") # for cld function
+  }
 library(ARTool)
 library(emmeans)
 library(multcompView)
@@ -11,30 +14,30 @@ library(multcompView)
 ### Step 2: Create Example Data
 
 
-data <- Results %>% select(Result_Result_Genotype,Result_Type, Result_AUDPC)
+data <- Results %>% select(Result_Genotype,Result_Type, Result_AUDPC)
 
 rownames(data) <- NULL
 
 ### Step 3: Perform Aligned Rank Transform
 
 # Perform the aligned rank transform
-art_model <- art(Result_AUDPC ~ Result_Result_Genotype * Result_Treatment, data = data)
+art_model <- art(Result_AUDPC ~ Result_Genotype * Result_Type, data = data)
 anova(art_model)
 
 # After step 3 eemeans will not work - try this modifacation (you will need to modify this for your data)
 #Extract estimates 
 table<-art_model$estimated.effects
 # Extract aligned ranks
-data$aligned_ranks <- art_model$residuals + table$`Result_Genotype:Result_Treatment`
+data$aligned_ranks <- art_model$residuals + table$`Result_Genotype:Result_Type`
 # Fit a linear model on the aligned ranks
-lm_model <- lm(data $aligned_ranks ~ Result_Genotype * Result_Treatment, data = data)
+lm_model <- lm(data $aligned_ranks ~ Result_Genotype * Result_Type, data = data)
 summary(lm_model)
 
 # Calculate estimated marginal means and perform pairwise comparisons
-emmeans_results <- emmeans(lm_model, ~ Result_Genotype * Result_Treatment)
+emmeans_results <- emmeans(lm_model, ~ Result_Genotype * Result_Type)
 pairwise_comparisons <- pairs(emmeans_results)
 summary(pairwise_comparisons)
-# Obtain compact letter display (cld) to group Result_Treatments
+# Obtain compact letter display (cld) to group Result_Types
 cld_results <- cld(emmeans_results, Letters = letters)
 print(cld_results)
 
