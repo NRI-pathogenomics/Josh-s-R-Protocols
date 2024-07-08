5e03255e6241d9381d729dec1d4cae4434d4d993
yourGitHubUsername <jjh62@kent.ac.uk>
2024-07-08 18:34:18 +0100

    left-joined the cld_results to the data dataframe and debugged the cld_results function as it was renaming the Col0 groups to Col meaning the left join wasn't working for the Col-0 groups

--- Kruskal-Wallis Test/Kruskal Wallis Test.R
+++ Kruskal-Wallis Test/Kruskal Wallis Test.R
@@ -63,27 +63,17 @@ cld_result <- cldList(data=dunn_result,
                       formula =  P ~ comparisons, 
                       comparison=comparisons, 
                       threshold = 0.05, 
-                      print.comp = TRUE)
+                      print.comp = TRUE, remove.zero = FALSE)
+#Remove.zero is important as it prevents the Col0 group from becoming Col allowing for a normal left_join to occur
 print(cld_result)
 # Convert CLD results to a data frame (ensures it's in the correct format)
 cld_df <- as.data.frame(cld_result)
 
-# # Ensure that the grouping factor is ordered correctly
-# cld_df$Result_Genotype <- factor(cld_df$Result_Genotype, levels = unique(cld_df$Result_Genotype))
-# cld_df$Result_Type <- factor(cld_df$Result_Type, levels = unique(cld_df$Result_Type))
-# # this groups the results in the desired manner and only selects the unique values for each treatment type and genotype
-# 
-# # Rename columns for clarity (optional)
-# colnames(cld_df)[colnames(cld_df) == ".group"] <- "CLD_Group"
-# 
-# # Create the bar plot
-# KW_plot <- ggplot(cld_df, aes(x = Result_Type, y = emmean, fill = Result_Genotype)) +
-#   geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.7) +
-#   geom_errorbar(aes(ymin = emmean - SE, ymax = emmean + SE),
-#                 position = position_dodge(width = 0.8), width = 0.25) +
-#   geom_text(aes(label = CLD_Group, y = emmean + SE + 0.1),
-#             position = position_dodge(width = 0.8), vjust = 0) +
-#   labs(x = "Result Type", y = "Estimated Marginal Means", fill = "Result Genotype",
-#        title = "Bar Plot with Compact Letter Display") +
-#   theme_minimal()
-# print(art_plot)
+#Add the Cld results to the "data" dataframe
+#First, some reshaping of the data dataframe
+data$Group <- paste(data$Result_Genotype, data$Result_Type, sep=".")
+
+# now left join the cld_result to the data data_frame
+data <- data %>% left_join(cld_result,by="Group")
+print(data)
+
