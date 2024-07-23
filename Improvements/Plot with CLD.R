ce13acfbdd65e03448c0eded30e31ec25191bdf1
yourGitHubUsername <jjh62@kent.ac.uk>
2024-07-23 15:07:29 +0100

    Added the Average_AUDPC values and CLD

--- Improvements/Plot with CLD.R
+++ Improvements/Plot with CLD.R
@@ -1,7 +1,8 @@
 #plot the data with cld
 #fill is based on Treatment so there's only need for 2 colours on the bar plot
 #geomerrobar uses the SE column to add the error bars
-
+Average_AUDPC
+CLD
 ggplot(data = Average_AUDPC, aes(x = Genotype, y = Average_AUDPC, fill = Treatment)) +
   geom_bar(stat = "identity", position = position_dodge(), width = 0.7) +
   geom_errorbar(aes(ymin = Average_AUDPC - SE, ymax = Average_AUDPC + SE),
