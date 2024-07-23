c21a0495dfd7a223a3b268f58e1b4f167f74ed8e
yourGitHubUsername <jjh62@kent.ac.uk>
2024-07-23 15:07:05 +0100

    Added new file to produce a second bar plot with the CLD labels.

---
+++ Improvements/Plot with CLD.R
@@ -0,0 +1,13 @@
+#plot the data with cld
+#fill is based on Treatment so there's only need for 2 colours on the bar plot
+#geomerrobar uses the SE column to add the error bars
+
+ggplot(data = Average_AUDPC, aes(x = Genotype, y = Average_AUDPC, fill = Treatment)) +
+  geom_bar(stat = "identity", position = position_dodge(), width = 0.7) +
+  geom_errorbar(aes(ymin = Average_AUDPC - SE, ymax = Average_AUDPC + SE),
+                position = position_dodge(0.7), width = 0.25) +
+  labs(title = "Average AUDPC with Standard Error by Genotype and Treatment",
+       x = "Genotype",
+       y = "Average AUDPC") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
