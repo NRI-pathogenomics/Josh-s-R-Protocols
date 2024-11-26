0e107f27c0c3a02337514aaab1320b06f0a7c19d
yourGitHubUsername <jjh62@kent.ac.uk>
2024-11-26 15:36:00 +0000

    Re-introduced the outlier removal functiion but changed the analysis steps so tha while cleaned_data is still generated, the original data dataframe is what the tests are performed on. Note to self: The kruskal wallis test calculates the difference between the groups, the dunn test actually indicates which groups are different which cldList then uses to give each group a letter. At the end of this script the cld ranks are applied to each indivdual replicate for further downstream analysis, though the dunn results are based on the AUDPC values which are the average for each group.

--- Kruskal-Wallis Test/Kruskal Wallis Test.R
+++ Kruskal-Wallis Test/Kruskal Wallis Test.R
@@ -9,6 +9,7 @@
 # install.packages("tidyverse")# For wrangling and tidying the data
 # install.packages("dunn.test")
 # install.packages("rcompanion")
+# install.packages("dplyr")
 library(MASS)
 library(tidyverse)
 library(dplyr)
@@ -29,7 +30,7 @@ library(FSA)
 library(tidyverse)
 library(ggpubr)
 
-data <- Results %>% select(Result_Genotype,Result_Type, Result_AUDPC)
+data <- Results |> dplyr::select(Result_Genotype, Result_Type, Result_AUDPC)
 
 rownames(data) <- NULL
 
@@ -42,40 +43,40 @@ data$Result_Type <- as.factor(unlist(data$Result_Type))
 # remove outliers from the data
 
 # # Function to remove outliers based on IQR
-# remove_outliers <- function(df) {
-#   Q1 <- quantile(df$Result_AUDPC, 0.25)
-#   Q3 <- quantile(df$Result_AUDPC, 0.75)
-#   IQR <- Q3 - Q1
-#   
-#   lower_bound <- Q1 - 1.5 * IQR
-#   upper_bound <- Q3 + 1.5 * IQR
-#   
-#   df %>% filter(Result_AUDPC >= lower_bound & Result_AUDPC <= upper_bound)
-# }
+remove_outliers <- function(df) {
+  Q1 <- quantile(df$Result_AUDPC, 0.25)
+  Q3 <- quantile(df$Result_AUDPC, 0.75)
+  IQR <- Q3 - Q1
 
-# Apply the function to each group
+  lower_bound <- Q1 - 1.5 * IQR
+  upper_bound <- Q3 + 1.5 * IQR
+
+  df %>% filter(Result_AUDPC >= lower_bound & Result_AUDPC <= upper_bound)
+}
+
+# # Apply the function to each group
 cleaned_data <- data %>%
   group_by(Result_Genotype) %>%
   group_modify(~remove_outliers(.x)) %>%
   ungroup()
 
-# Check the result
+# # Check the result
 print(cleaned_data)
 
 
 ### Step 3: Perform Kruskal-Wallis Test
 #On the data overall
 print("Mock vs Inoculated")
-KW_results1 <- kruskal.test(Result_AUDPC, formula=Result_AUDPC ~ Result_Genotype * Result_Type, data = cleaned_data)
+KW_results1 <- kruskal.test(Result_AUDPC, formula=Result_AUDPC ~ Result_Genotype * Result_Type, data = data)
 print(KW_results1)
 
 # Kruskal-Wallis Test
 print("Mock vs Inoculated")
-KW_results1 <- kruskal.test(Result_AUDPC, Result_AUDPC ~ Result_Genotype * Result_Type, data = cleaned_data)
+KW_results1 <- kruskal.test(Result_AUDPC, Result_AUDPC ~ Result_Genotype * Result_Type, data = data)
 print(KW_results1)
 
 # Post Hoc: Perform Dunn's Test
-dunn_result <- dunn.test(cleaned_data$Result_AUDPC, g=interaction(cleaned_data$Result_Genotype, cleaned_data$Result_Type), method="bonferroni")
+dunn_result <- dunn.test(data$Result_AUDPC, g=interaction(data$Result_Genotype, data$Result_Type), method="bonferroni")
 print("Post-Hoc Dunn Test")
 print(dunn_result)
 
@@ -89,15 +90,17 @@ cld_result <- cldList(data=dunn_result,
                       threshold = 0.05, 
                       print.comp = TRUE, remove.zero = FALSE)
 #Remove.zero is important as it prevents the Col0 group from becoming Col allowing for a normal left_join to occur
+#the cldList function ranks the results of the Dunn test which indicates which groups are different as the KW results, while it statistically calculates the difference...
+#... it does not indicate the differences in the results
 print(cld_result)
 # Convert CLD results to a data frame (ensures it's in the correct format)
 cld_df <- as.data.frame(cld_result)
 
 #Add the Cld results to the "data" dataframe
 #First, some reshaping of the data dataframe
-data$Group <- paste(cleaned_data$Result_Genotype, cleaned_data$Result_Type, sep=".")
+data$Group <- paste(data$Result_Genotype, data$Result_Type, sep=".")
 
-# now left join the cld_result to the data data_frame
-cleaned_data <- cleaned_data %>% left_join(cld_result,by="Group")
-print(cleaned_data)
+# now left join the cld_result to the data data_frame to assign the cld rank to individual replicates
+data <- data %>% left_join(cld_result,by="Group")
+print(data)
 
