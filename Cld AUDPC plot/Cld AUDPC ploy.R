1d9a124eca78074917abebb2600871426a52044e
yourGitHubUsername <jjh62@kent.ac.uk>
2024-12-02 17:30:34 +0000

    Adjusted the script to produce a cld if a non-parametric test is needed. reorganised the cld_df to contain the values in the same order they come in the combobarplotdata so they can be merged after combobarplotdata is melted into combobarplotdata_long. the adjusted _long df is then used in the geomtext() aesthetics parameter from ggplot to add the cld to the bar plot. SE values and SE bars are unaffected.

--- Cld AUDPC plot/Cld AUDPC ploy.R
+++ Cld AUDPC plot/Cld AUDPC ploy.R
@@ -1,11 +1,89 @@
 # cld_AUDPC Script
 # Redoes the AUDPC plot with the clustered letter display data
+library(dplyr)
 # create a new cld_df with the dataframe in the same order they are in the combobarplot_long df
-melt_df <- cld_df[order(cld_df$Group), ]
+melt_df <- cld_df %>% 
+  arrange(desc(grepl("\\.M$", Group)), Group)
+
+combo_barplotdata <- rbind(Mock_column, Inno_column)
+
+
+# When it comes to the Standard Error I need to filter through the individual AUDPC replicates data to get the number of observations per per genotype
+# add a number of replicates per treatment to the data set
+
+# Calculate Standard Error:
+
+standard_error_data <- list()
+
+# Reshape data into long format
+combo_barplotdata_long <- melt(combo_barplotdata, id.vars = "Genotype + Treatment")
+combo_barplotdata$`Genotype + Treatment` <- as.factor(as.character(combo_barplotdata$`Genotype + Treatment`))
+
+# Check the structure of Genotypes
+str(combo_barplotdata_long$`Genotype + Treatment`)
+# numerical data 
+combo_barplotdata_long$value <- as.numeric(combo_barplotdata_long$value)
+
+# Check the structure of Genotypes
+str(combo_barplotdata_long$`Genotype + Treatment`)
+# convert value to numerical data 
+combo_barplotdata_long$value <- as.numeric(combo_barplotdata_long$value)
+
+#use the loop reliably calculate the SE value for each treatment using the INDVIDUAL AUDPC data
+
+# In the loop create a subset of the Results data frame, first for the Mock then for the Innoculated
+
+standard_error_data <- list()
+mock_se <- list()
+inno_se <- list()
+for(i in 1:GenoNumber){
+  #Mock
+  subset2 <- subset(Results, Result_Genotype == Genotypes[i])
+  subset2 <- subset(subset2, Result_Type == "M")
+  print(subset2)
+  mock.st.dev <- sd(as.numeric(subset2$Result_AUDPC))
+  print(paste("Standard Deviation: ", mock.st.dev))
+  mock.no.of.reps <- length(subset2$Result_Type) 
+  mock.no.of.reps <- mock.no.of.reps + 1
+  print(paste("number of reps: ", mock.no.of.reps))
+  mock.SE.value <- mock.st.dev/sqrt(mock.no.of.reps)
+  print(paste("SE value ",mock.SE.value))
+  mock_se <- append(mock_se, mock.SE.value)
+  #Inocculated
+  subset3 <- subset(Results, Result_Genotype == Genotypes[i])
+  subset3 <- subset(subset3, Result_Type == "I")
+  print(subset3)
+  inno.st.dev <- sd(as.numeric(subset3$Result_AUDPC))
+  print(paste("Standard Deviation: ", inno.st.dev))
+  inno.no.of.reps <- length(subset3)
+  inno.no.of.reps <- inno.no.of.reps + 1
+  print(paste("number of reps: ", inno.no.of.reps))
+  print(inno.no.of.reps)
+  inno.SE.value <- inno.st.dev/sqrt(inno.no.of.reps)
+  print(paste("SE value ",inno.SE.value))
+  inno_se <- append(inno_se, inno.SE.value)
+}
+standard_error_data <- append(mock_se, inno_se)
+## Corrected Bar Plot
+
+# melt function from reshape2 package converts the data from a wide to a long format (A long format contains values that do repeat in the first column)
+# Plot the side-by-side stacked bar plot
+
+## Corrected Bar Plot
+
+# melt function from reshape2 package converts the data from a wide to a long format (A long format contains values that do repeat in the first column)
+# Plot the side-by-side stacked bar plot
 
 # Merge Standard Error data back into combo_barplotdata_long dataset
-combo_barplotdata_long$cld <- melt_df$MonoLetter
+combo_barplotdata_long$SE <- standard_error_data
 
+#convert SE to numeric data
+
+combo_barplotdata_long$SE <- as.numeric(combo_barplotdata_long$SE)
+
+#add cld values to the combobarplotdata_long df
+
+combo_barplotdata_long$cld <- melt_df$MonoLetter
 
 # Get unique levels of Treatment
 treatment_levels <- unique(combo_barplotdata_long$`Genotype + Treatment`)
