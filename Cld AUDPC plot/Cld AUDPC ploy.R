8fdb1c162400899b88cc864f012c69059b03136d
yourGitHubUsername <jjh62@kent.ac.uk>
2024-12-16 10:10:02 +0000

    Added new AUDPC script for the P.syringae data
    Merge branch 'main' of https://github.com/NRI-pathogenomics/Josh-s-R-Protocols
    
    # Conflicts:
    #	.DS_Store

--- Cld AUDPC plot/Cld AUDPC ploy.R
+++ Cld AUDPC plot/Cld AUDPC ploy.R
@@ -100,11 +100,14 @@ AUDPC_Plot <- ggplot(data = combo_barplotdata_long, aes(x = `Genotype + Treatmen
   geom_bar(stat = "identity", position = position_dodge(width = 1)) +
   labs(title = "AUDPC of Mock vs Inoculated",
        x = "Genotypes", y = "Average AUDPC", fill = "Treatment") +
-  scale_fill_manual(values = color_mapping) +  # Use color_mapping
+  scale_fill_manual(values = color_mapping) +
   geom_errorbar(aes(ymin = value - SE, ymax = value + SE), position = position_dodge(width = 1), 
                 width = 0.2, colour = "black", alpha = 0.5, linewidth = 0.5) +
-  theme(axis.text.x = element_blank())  # Remove x-axis labels
-
+  geom_text(aes(label = cld, y = value + SE + 1), 
+            position = position_dodge(width = 1), 
+            vjust = -0.5) +
+  theme(axis.text.x = element_blank())
 show(AUDPC_Plot)
 
 #geomerrorbar() needs to be used but it needs to be able to work on both bars
+#geom_text() has been added using the new cld coloumn in combobarplot_long to add the cld values to the plot 
