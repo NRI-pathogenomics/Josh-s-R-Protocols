45f9c24c6a81c25167a3b7c0fc9a49402b52deb4
yourGitHubUsername <jjh62@kent.ac.uk>
2024-12-16 10:33:13 +0000

    Started rewriting the code for P.syringae infiltration data - it had a lot of redundancies in it an so I'm, optimising the code at the same time.

--- P.syringae Tests/AUDPC P.syringae.R
+++ P.syringae Tests/AUDPC P.syringae.R
@@ -21,78 +21,45 @@ library(reshape2)
 library(tidyverse)
 library(magrittr)
 library(plotrix)
-Path_Assay <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/Input Files/NEWPT2.csv", 
+Path_Assay <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/P.syringae Tests/Disease severity Scores.csv", 
                        header = TRUE, sep = ",", quote = "\"",
                        dec = ".", fill = TRUE, comment.char = "")
-Path_Assay <- setNames(Path_Assay, nm=c("Genotype", "block",	"treatment.", "number", "Day 28", "Day 35", "Day 49", "Day 56"))
+Path_Assay <- setNames(Path_Assay, nm=c("Replicate", "Treatment", "Block", "Day 0", "Day 1", "Day 2", "Day 3", "Day 5"))
 
 
 if(file.exists("/Users/joshhoti/Library/CloudStorage/
-               OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/Combined Average plots/Mock Averaged Results")==FALSE){
+               OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/P.syringae Tests/Mock Averaged Results")==FALSE){
   file.create("Mock Averaged Results")
 }
 
 if(file.exists("/Users/joshhoti/Library/CloudStorage/
-               OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/Combined Average plots/Innoculated Averaged Results")==FALSE){
+               OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/P.syringae Tests/Innoculated Averaged Results")==FALSE){
   file.create("Innoculated Averaged Results")
 }
 ##Subsetting the Data
-Inno_Sub <- subset(Path_Assay, treatment.=="I")
-Mock_Sub <- subset(Path_Assay, treatment.=="M")
+Inno_Sub <- subset(Path_Assay, Treatment=="I")
+Mock_Sub <- subset(Path_Assay, Treatment=="M")
 ##Individual AUDPC
 Tail_Value <- Path_Assay$block[length(Path_Assay$block)]
 
-setNames(Path_Assay, nm=c("Genotype", "block",	"treatment.", "number", "Day 28", "Day 35", "Day 49", "Day 56"))
+setNames(Path_Assay, nm=c("Replicate", "Treatment", "Block", "Day 0", "Day 1", "Day 2", "Day 3", "Day 5"))
 
-## Individual AUDPCs
-Result_Genotype <- list()
-Result_Treatment <- list()
-Result_AUDPC <-list()
-Result_Type <- list()
-for(i in 1:Tail_Value){
-  Treatment_Block <- as.numeric(i)
-  Treatment_value <- paste0("Treatment Block: ", i)
-  print(Treatment_value)
-  Block <- subset(Path_Assay, block==i)
-  # print(evaluation)
-  # Next step is to plot multiple lines on the same graph by seperating them by Genotype and Treatment 
-  # (using another for loop selecting the column in evalutation using evaluation$column)
-  # print("Mock Group: ")
-  Mock <- subset(Block, treatment.=="M")
-  Genotypes <- c(unique(Mock$Genotype))
-  GenoNumber <- length(Genotypes)
-  MockAUDPC <- list()
-  for(i in 1:GenoNumber){
-    Mockotype <- subset(Mock, Genotype==Genotypes[i])
-    #Calculate audpc using agricolae on the subset then leave room for the plots, same for innoculated
-    dates <- c(28,35,49,56)
-    evaluation <- unlist(Mockotype[5:8], use.names=FALSE)
-    AUDPC <- audpc(evaluation, dates, type="absolute")
-    #print(paste0("AUDPC for ",Genotypes[i], " is: ", AUDPC))
-    Result_Genotype <- append(Result_Genotype, Genotypes[i])
-    Result_Treatment <- append(Result_Treatment,Treatment_Block)
-    Result_AUDPC <-append(Result_AUDPC,AUDPC)
-    Result_Type <- append(Result_Type, Mockotype$treatment.)
-    #Insert ggplot2 code here
+# ## Individual AUDPCs
+# Result_Replicate <- list()
+# Result_Treatment <- list()
+# Result_AUDPC <-list()
+# Result_Block <- list()
+
+#Calculate audpc using agricolae on the subset then leave room for the plots, same for innoculated
+
+MockLength <- length(Mock_Sub$Replicate)
+Mock_AUDPC <- list()
+for i in 1:MockLength{
+  dates <- c(0,1,2,3,5)
+  evaluation <- unlist(Mock_Sub[4:8], use.names=FALSE)
+  AUDPC <- audpc(evaluation, dates, type="absolute")
   }
-  Innoculated <- subset(Block, treatment.=="I")
-  Genotypes <- c(unique(Innoculated$Genotype))
-  GenoNumber <- length(Genotypes)
-  # print("Innoculated Group: ")
-  for(i in 1:GenoNumber){
-    Innotype <- subset(Innoculated, Genotype==Genotypes[i])
-    #Calculate audpc using agricolae on the subset then leave room for the plots, same for innoculated
-    dates <- c(28,35,49,56)
-    evaluation <- unlist(Mockotype[5:8], use.names = FALSE)
-    AUDPC <- audpc(evaluation, dates, type="absolute")
-    #print(paste0("AUDPC for ",Genotypes[i], " is: ", AUDPC))
-    Result_Genotype <- append(Result_Genotype, Genotypes[i])
-    Result_Treatment <- append(Result_Treatment,Treatment_Block)
-    Result_AUDPC <-append(Result_AUDPC,AUDPC)
-    Result_Type <- append(Result_Type, Innotype$treatment.)
-  }
-  
-}
+
 #Generate the Results and reform the original data frame
 Results <- cbind(Result_Genotype,Result_Type,Result_Treatment, Result_AUDPC)
 Results <- as.data.frame(Results, row.names = NULL)
