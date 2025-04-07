970ebd761481357c704059efdbc8fe9f0ca34ff7
yourGitHubUsername <jjh62@kent.ac.uk>
2025-04-07 17:46:08 +0100

    Wrote a new model that tests the importance of each factor seperately over a null model and calcualtes likelihood ratios (measuring difference in fit), P.values (probability of observing the likelihood ratios calculated, by chance) and Akaike Information Criterion (the balance between model fit and complexity). ANOVA tests compare the models each to the null model, the results for LR, AIC and P-value can then be used to determine which model fits better and thus which factor is more strongly linked to leaf damager

--- Combined Infiltrations/Batch 1/Col-0 Batch 1 Mixed Model Analysis.R
+++ Combined Infiltrations/Batch 1/Col-0 Batch 1 Mixed Model Analysis.R
@@ -1,1 +1,37 @@
 # Col-0 Batch 1 Mixed Model Analysis
+install.packages("ordinal")
+library(ordinal)
+# since the response variables are ordinal data
+disease_scores <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/Combined Infiltrations/Batch 1/Batch 1 Scores.csv", 
+                           header = TRUE, sep = ",", quote = "\"",
+                           dec = ".", fill = TRUE, comment.char = "")
+# data processing
+disease_scores <- na.omit(disease_scores) #removes NA values
+disease_scores <- subset(disease_scores, select = -c(Chlorosis,Random, Block.Rep)) 
+disease_scores <- subset(disease_scores, Perforation == "Y")
+#removes chlorosis as this test is focusing on leaf damage
+#randomized block design calculations and the block rep (not important for this model)
+
+#The model - models for individual factor (P.syringae infiltration and fungus gnats) to determine which of the two is more closely associated with leaf damage
+
+# Model with only P. syringae (hypothesis: P.syringae are more closely associated with damage)
+model_ps <- clm(as.ordered(Leaf.Damage) ~ Infiltrated.with.P.syringae, 
+                data = disease_scores)
+
+# Model with only fungus gnats (hypothesis: fungus gnats are more closely associated with damage)
+model_fg <- clm(as.ordered(Leaf.Damage) ~ Fungus.gnats, 
+                data = disease_scores)
+
+# Null model (hypothesis: neither factor is more closely associated with damage)
+model_null <- clm(as.ordered(Leaf.Damage) ~ 1, 
+                  data = disease_scores)
+
+# Compare using likelihood ratio tests using a default P.value of < 0.05
+anova(model_null, model_ps)
+anova(model_null, model_fg)
+
+#summary to quantify the effect size of each factor
+summary(model_ps)
+summary(model_fg)
+
+
