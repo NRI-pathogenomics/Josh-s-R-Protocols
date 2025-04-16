373dcf6065d15e762d6da54bb27157a7e3f1f95b
yourGitHubUsername <jjh62@kent.ac.uk>
2025-04-16 10:29:27 +0100

    Created new Mixed Model Analysis and Rot Scores for Col-0 Batch 3

---
+++ Combined Infiltrations/Batch 3/Col-0 Batch 3 Mixed Model Analysis.R
@@ -0,0 +1,134 @@
+# Col-0 Batch 3 Mixed Model Analysis
+# # Install and load required package
+# if(!require(vcd)) install.packages("vcd")
+# library(vcd)
+# 
+# # Calculate association measures
+# assocstats(table(disease_scores$Treatment, disease_scores$Infiltrated.with.P.syringae))
+# assocstats(table(disease_scores$Treatment, disease_scores$Fungus.gnats))
+# assocstats(table(disease_scores$Treatment, disease_scores$Perforation))
+# if(("dplyr" %in% all_packages)==FALSE){
+#   install.packages("dplyr")}
+# if(("agricolae" %in% all_packages)==FALSE){
+#   install.packages("agricolae")}
+# if(("agricolaeplotr" %in% all_packages)==FALSE){
+#   install.packages("agricolaeplotr")}
+# if(("tidyverse" %in% all_packages)==FALSE){
+#   install.packages("tidyverse")}
+# if(("plotrix" %in% all_packages)==FALSE){
+#   install.packages("plotrix")}
+# if(("FSA" %in% all_packages)==FALSE){
+#   install.packages("FSA")}
+# if(("dunn.test" %in% all_packages)==FALSE){
+#   install.packages("dunn.test")}
+# if(("rcompanion" %in% all_packages)==FALSE){
+#   install.packages("rcompanion")}
+# install.packages("ordinal")
+# install.packages("multcompView")
+library(multcompView)
+library(rcompanion)
+library(ordinal)
+library(lme4)
+# since the response variables are ordinal data
+disease_scores <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/Combined Infiltrations/Batch 1/Batch 1 Scores.csv", 
+                           header = TRUE, sep = ",", quote = "\"",
+                           dec = ".", fill = TRUE, comment.char = "")
+# data processing
+disease_scores <- na.omit(disease_scores) #removes NA values
+disease_scores <- subset(disease_scores, select = -c(Chlorosis,Random, Block.Rep)) 
+disease_scores$Treatment <- gsub("-", ".", disease_scores$Treatment)
+disease_scores <- subset(disease_scores, Treatment != "Ctrl.HP.I")
+#removes chlorosis as this test is focusing on leaf damage
+#randomized block design calculations and the block rep (not important for this model)
+
+# #The model - models for individual factors (Perforation, P.syringae infiltration and fungus gnats) to determine which of the two is more closely associated with leaf damage
+# 
+# # Model with only P. syringae (hypothesis: P.syringae are more closely associated with damage)
+# model_ps <- clm(as.ordered(X5.dpi.Leaf.Damage) ~ Infiltrated.with.P.syringae, 
+#                 data = disease_scores)
+# 
+# # Model with only fungus gnats (hypothesis: fungus gnats are more closely associated with damage)
+# model_fg <- clm(as.ordered(X5.dpi.Leaf.Damage) ~ Fungus.gnats, 
+#                 data = disease_scores)
+# 
+# # Model with only Perforation (hypothesis: Infiltration process is closely associated with damage)
+# model_pf <- clm(as.ordered(X5.dpi.Leaf.Damage) ~ Perforation, 
+#                 data = disease_scores)
+
+#Complete Model
+lmer1a <- lmer(X5.dpi.Leaf.Damage ~ Treatment + Infiltrated.with.P.syringae + (1 | Block) + (1 | Fungus.gnats) + (1 | Perforation) + (1|X0.dpi.Leaf.Damage), data = disease_scores)
+summary(lmer1a)
+
+# Simpler Model (dropped X0.dpi.Leaf.Damage as a random effect)
+
+lmer1b <- lmer(X5.dpi.Leaf.Damage ~ Treatment + Infiltrated.with.P.syringae + (1 | Block) + (1 | Fungus.gnats) + (1 | Perforation), data = disease_scores)
+summary(lmer1b)
+
+anova(lmer1a, lmer1b)
+
+# Simpler Model (dropped Infiltrated.with.P.syringae)
+lmer1c <- lmer(X5.dpi.Leaf.Damage ~ Treatment + (1 | Block) + (1 | Fungus.gnats) + (1 | Perforation), data = disease_scores)
+summary(lmer1b)
+
+# Simpler Model (dropped Block as a Random effect)
+
+lmer1d <- lmer(X5.dpi.Leaf.Damage ~ Treatment + (1 | Fungus.gnats) + (1 | Perforation), data = disease_scores)
+summary(lmer1c)
+
+anova(lmer1b,lmer1c)
+# compare whether meodle with block is significantly different to the model without - if it is then Block is important 
+#It is in this case
+
+lmer1e <- lmer(X5.dpi.Leaf.Damage ~ Treatment + (1 | Block) + (1 | Perforation), data = disease_scores)
+summary(lmer1e)
+anova(lmer1b,lmer1d)
+#Compare the model with Block and Fungus.gnats to the model with Block only
+#P.value indicates Fungus gnats is importance
+#Simpler model without the Perforation
+
+lmer1f <- lmer(X5.dpi.Leaf.Damage ~ Treatment + (1 | Block) + (1 | Fungus.gnats), data = disease_scores)
+summary(lmer1f)
+anova(lmer1d,lmer1e)
+#Result indicates that perforation is not important - perforation does not explain a significant amount of variance in leaf damage
+
+#Simplier model without Treatment
+
+lmer1g <- lmer(X5.dpi.Leaf.Damage ~ (1 | Block) + (1 | Fungus.gnats), data = disease_scores)
+anova(lmer1f, lmer1g)
+
+# Results indicate that treatment does explain a significant amount of variance in leaf damage
+
+#best fit model:
+summary(lmer1g)
+
+#No we need a post-hoc test to determine which treatments differ from one another:
+# the magnitude of T-values indicates that at least one of the treatments differs from the others in terms of Leaf Damage but the post-hoc test will reveal which treatment differs
+#In short: Treatment is a fixed effect and one or more treatment is behind the leaf damage
+#Fungus.gnat damage is accounted for as a random effect that causes variance in the data
+# but depending on the results of the post-hoc test, if one of the infected/agroinfiltrated treatments differs signficantly from the others then it/they are the reason for leaf degradation - ergo the stats will show if P.syringae presence is behind leaf degredation
+
+# Null model (hypothesis: neither factor is more closely associated with damage)
+model_null <- clm(as.ordered(X5.dpi.Leaf.Damage) ~ 1, 
+                  data = disease_scores)
+
+# # Compare using likelihood ratio tests using a default P.value of < 0.05
+# anova(model_null, model_ps)
+# anova(model_null, model_pf)
+# anova(model_null, model_fg)
+# 
+# #summary to quantify the effect size of each factor
+# summary(model_ps)
+# summary(model_pf)
+# summary(model_fg)
+# # Remodel to look at the interaction between treatment and whether it was infiltrated with P.syringae, accounting for leaf damage caused by fungus gnats
+
+#Post Hoc Test - Tukey Test
+library(emmeans)
+posthoc <- emmeans(lmer1f, ~ Treatment)
+summary(posthoc)
+tukey_results <- pairs(posthoc, adjust = "tukey")
+summary(tukey_results)
+tukey_results <- as.data.frame(tukey_results)
+disease_treatments_cld <- cldList(p.value ~ contrast, data = tukey_results, threshold = 0.05)
+# Change EHA15.I to EHA105.I
+disease_treatments_cld$Group[disease_treatments_cld$Group == "EHA15.I"] <- "EHA105.I"
---
+++ Combined Infiltrations/Batch 3/Rot Scores.R
@@ -0,0 +1,200 @@
+#Disease Assay Average Scores 
+#Batch 3
+# all_packages <- installed.packages()
+# 
+# if(("dplyr" %in% all_packages)==FALSE){
+#   install.packages("dplyr")}
+# if(("agricolae" %in% all_packages)==FALSE){
+#   install.packages("agricolae")}
+# if(("agricolaeplotr" %in% all_packages)==FALSE){
+#   install.packages("agricolaeplotr")}
+# if(("tidyverse" %in% all_packages)==FALSE){
+#   install.packages("tidyverse")}
+# if(("plotrix" %in% all_packages)==FALSE){
+#   install.packages("plotrix")}
+# if(("FSA" %in% all_packages)==FALSE){
+#   install.packages("FSA")}
+# if(("dunn.test" %in% all_packages)==FALSE){
+#   install.packages("dunn.test")}
+# if(("rcompanion" %in% all_packages)==FALSE){
+#   install.packages("rcompanion")}
+
+library(agricolae)
+library(agricolaeplotr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(reshape2)
+library(tidyverse)
+library(magrittr)
+library(plotrix)
+library(FSA)
+library(dunn.test)
+library(rcompanion)
+
+Path_Assay <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/Combined Infiltrations/Batch 2/Batch 2 Scores.csv", 
+                       header = TRUE, sep = ",", quote = "\"",
+                       dec = ".", fill = TRUE, comment.char = "")
+## Leaf Damage
+# Pre-process the data
+LD_Assay <- na.omit(Path_Assay)
+LD_Assay <- subset(LD_Assay, select = -c(Random, Block.Rep)) #remove randomized block design calculations
+LD_Assay$Treatment <- gsub("-", ".", LD_Assay$Treatment)
+## Calculate the Average Leaf Damage of each treatment
+# take the individual treatment names
+treatments <- unique(LD_Assay$Treatment)
+#get the number of treatments
+x <- length(treatments)
+#Create Leaf Damage (LD) Averages data frame
+# Define the column names
+column_names <- c("Treatments", "Leaf Damage Averages")
+
+# Create an empty data frame with the specified column names
+LD.Averages <- data.frame(matrix(ncol = length(column_names), nrow = x))
+colnames(LD.Averages) <- column_names
+#add the treatments to the Averages Data frame
+LD.Averages$Treatments <- as.data.frame(treatments)
+# Display the empty data frame
+print(LD.Averages)
+for(i in 1:x){
+  Data_Sub <- subset(LD_Assay, Treatment == treatments[i])
+  Data_Sub
+  ld.mean <- mean(as.numeric(Data_Sub$Leaf.Damage))
+  LD.Averages$`Leaf Damage Averages`[i] <- ld.mean
+}
+
+
+##Plot Data Here
+#Standard Error
+# Standard Error for  Leaf.Damage
+for(i in 1:x){
+  Data_Sub <- subset(LD_Assay, Treatment == treatments[i])
+  Ld.SE <- sd(Data_Sub$Leaf.Damage, na.rm = TRUE) / sqrt(length(Data_Sub$Leaf.Damage))
+  LD.Averages$SE[i] <- Ld.SE
+}
+
+
+# Load required library
+library(ggplot2)
+library(reshape2)
+
+
+
+# Convert data and SE values to long format
+# If you haven't already installed reshape2
+
+# add cld letters to LD.averages
+
+
+LD.Averages <- LD.Averages %>%
+  arrange(Treatments)
+
+leaf_damage_cld <- leaf_damage_cld %>%
+  arrange(`Group`)
+
+LD.Averages <- cbind(LD.Averages, leaf_damage_cld$Letter)
+column_names <- c("Treatments", "Leaf Damage Averages", "SE", "CLD")
+colnames(LD.Averages) <- column_names
+# Plot with CLD labels
+# To make sure ggplot2 treats the treatments in the correct order and discrete categories, convert it to factor like this:
+LD.Averages$Treatments <- unlist(LD.Averages$Treatments)
+LD.Averages$Treatments <- factor(LD.Averages$Treatments)
+#ggplot needs a character vector for axes values but directly converting Treatments to a factor causes ggplot to interpret Treatments as one value
+# so the column needs to be converted into characters data type first and then converted to a factor - making Treatments a character vector
+
+# Plot +
+LD_plot <- ggplot(LD.Averages, aes(x = Treatments, y = `Leaf Damage Averages`, fill = Treatments)) +
+  geom_col(color = "black") +
+  geom_errorbar(aes(ymin = `Leaf Damage Averages` - SE, ymax = `Leaf Damage Averages` + SE), width = 0.2) +
+  geom_text(aes(label = CLD, y = `Leaf Damage Averages` + SE + 1), size = 5) +
+  theme_minimal() +
+  labs(y = "Average Leaf Damage Score", title = "Col-0 Batch 2 Average Leaf Damage Scores") +
+  scale_fill_brewer(palette = "Blues")  # nicer than all-blue
+
+show(LD_plot)
+
+## Chlorosis
+# Pre-process the data
+Chlorosis_Assay <- na.omit(Path_Assay)
+Chlorosis_Assay  <- subset(Chlorosis_Assay, select = -c(Random, Block.Rep)) #remove randomized block design calculations
+Chlorosis_Assay$Treatment <- gsub("-", ".", Chlorosis_Assay$Treatment)
+## Calculate the Average Chlorosis of each treatment
+# take the individual treatment names
+treatments <- unique(Chlorosis_Assay$Treatment)
+#get the number of treatments
+x <- length(treatments)
+#Create Leaf Damage (LD) Averages data frame
+# Define the column names
+column_names <- c("Treatments", "Chlorosis Averages")
+
+# Create an empty data frame with the specified column names
+Chlorosis.Averages <- data.frame(matrix(ncol = length(column_names), nrow = x))
+colnames(Chlorosis.Averages) <- column_names
+#add the treatments to the Averages Data frame
+Chlorosis.Averages$Treatments <- as.data.frame(treatments)
+# Display the empty data frame
+print(Chlorosis.Averages)
+for(i in 1:x){
+  Data_Sub <- subset(Chlorosis_Assay, Treatment == treatments[i])
+  Data_Sub
+  cl.mean <- mean(as.numeric(Data_Sub$Chlorosis))
+  print(cl.mean)
+  Chlorosis.Averages$`Chlorosis Averages`[i] <- cl.mean
+}
+
+
+##Plot Data Here
+#Standard Error
+# Standard Error for  Chlorosis
+for(i in 1:x){
+  Data_Sub <- subset(Chlorosis_Assay, Treatment == treatments[i])
+  cl.SE <- sd(Data_Sub$Chlorosis, na.rm = TRUE) / sqrt(length(Data_Sub$Chlorosis))
+  Chlorosis.Averages$SE[i] <- cl.SE
+}
+
+
+# Load required library
+library(ggplot2)
+library(reshape2)
+
+
+
+# Convert data and SE values to long format
+# If you haven't already installed reshape2
+# install.packages("reshape2")
+
+
+# add cld letters to LD.averages
+
+Chlorosis.Averages <- Chlorosis.Averages %>%
+  arrange(Treatments)
+
+chlorosis_cld <- chlorosis_cld %>%
+  arrange(`Group`)
+
+Chlorosis.Averages <- cbind(Chlorosis.Averages, chlorosis_cld$Letter)
+column_names <- c("Treatments", "Chlorosis Averages", "SE", "CLD")
+colnames(Chlorosis.Averages) <- column_names
+
+# Count the number of records for each Treatment type
+ld_treatment_counts <- table(LD_Assay$Treatment)
+cl_treatment_counts <- table(Chlorosis_Assay$Treatment)
+
+# Plot with CLD labels
+# To make sure ggplot2 treats the treatments in the correct order and discrete categories, convert it to factor like this:
+Chlorosis.Averages$Treatments <- unlist(Chlorosis.Averages$Treatments)
+Chlorosis.Averages$Treatments <- factor(Chlorosis.Averages$Treatments)
+#ggplot needs a character vector for axes values but directly converting Treatments to a factor causes ggplot to interpret Treatments as one value
+# so the column needs to be converted into characters data type first and then converted to a factor - making Treatments a character vector
+
+# Plot +
+CL_plot <- ggplot(Chlorosis.Averages, aes(x = Treatments, y = `Chlorosis Averages`, fill = Treatments)) +
+  geom_col(color = "black") +
+  geom_errorbar(aes(ymin = `Chlorosis Averages` - SE, ymax = `Chlorosis Averages` + SE), width = 0.2) +
+  geom_text(aes(label = CLD, y = `Chlorosis Averages` + SE + 1), size = 5) +
+  theme_minimal() +
+  labs(y = "Average Chlorosis Score", title = "Col-0 Batch 2 Average Chlorosis Scores") +
+  scale_fill_brewer(palette = "Reds")  # nicer than all-blue
+
+show(CL_plot)
+
