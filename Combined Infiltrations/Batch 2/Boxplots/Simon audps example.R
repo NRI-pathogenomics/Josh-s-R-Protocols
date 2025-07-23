545f4a17cad4d32e77a8b51cd4ec8db888e70b8f
yourGitHubUsername <jjh62@kent.ac.uk>
2025-07-23 12:17:19 +0100

    Completed the script - clds are added to the boxplots

--- Combined Infiltrations/Batch 2/Boxplots/Simon audps example.R
+++ Combined Infiltrations/Batch 2/Boxplots/Simon audps example.R
@@ -27,7 +27,7 @@ Batch_2$Treatment[Batch_2$Treatment == "Ctrl-HP-I"] <- "Ctrl.HP.I"
 Batch_2$Treatment[Batch_2$Treatment == "EHA105-I"] <- "EHA105.I"
 Batch_2$Treatment[Batch_2$Treatment == "Water-I"] <- "Water.I"
 Batch_2$Treatment[Batch_2$Treatment == "GFP-I"] <- "GFP.I"
-Batch_2$Treatment[Batch_2$Treatment == "GFP-I"] <- "GFP.M"
+Batch_2$Treatment[Batch_2$Treatment == "GFP-M"] <- "GFP.M"
 
 Batch_7 <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-UniversityofKent/Postgraduate/Josh R Protocols/Combined Infiltrations/Batch 7/Batch 7 Disease.csv", 
                     header = TRUE, sep = ",", quote = "\"",
@@ -37,7 +37,7 @@ Batch_7 <- subset(Batch_7, Treatment != "Ctrl.HP.I")
 Batch_7$Treatment[Batch_7$Treatment == "EHA105-I"] <- "EHA105.I"
 Batch_7$Treatment[Batch_7$Treatment == "Water-I"] <- "Water.I"
 Batch_7$Treatment[Batch_7$Treatment == "GFP-I"] <- "GFP.I"
-Batch_7$Treatment[Batch_7$Treatment == "GFP-I"] <- "GFP.M"
+Batch_7$Treatment[Batch_7$Treatment == "GFP-M"] <- "GFP.M"
 
 #Batch 1
 # Leaf Damage
@@ -76,37 +76,69 @@ box_audps
 #Batch 2
 # Leaf Damage
 B2_leaf_damage_cld
-box_audps <- ggplot(Batch_2, aes(x=Treatment, y=X5.dpi.Leaf.Damage, fill=Treatment)) +
+B2_leaf_damage_summary <- Batch_2 %>%
+  group_by(Treatment) %>%
+  summarise(Max = max(X5.dpi.Leaf.Damage)) %>%
+  left_join(B2_leaf_damage_cld, by = c("Treatment" = "Group"))
+
+box_audps <- ggplot(Batch_2, aes(x = Treatment, y = X5.dpi.Leaf.Damage, fill = Treatment)) +
   geom_boxplot() +
+  geom_text(data = B2_leaf_damage_summary,
+            aes(x = Treatment, y = Max + 0.5, label = Letter),  # adjust y offset as needed
+            size = 5, fontface = "bold") +
   theme_classic() +
-  labs(title="Col-0 Batch 2", subtitle = "Leaf Damage Scores 5 d.p.i",  x ="Treatment", legend = "Treatment") +
-  labs(y="Disease Index Scores 0-10")
+  labs(title = "Col-0 Batch 2", subtitle = "Leaf Damage Scores 5 d.p.i",
+       x = "Treatment", y = "Disease Index Scores 0-10")
 box_audps
-# Chlorosis
+# Chlorosis 
 B2_chlorosis_cld
-box_audps <- ggplot(Batch_2, aes(x=Treatment, y=X5.dpi.Chlorosis, fill=Treatment)) +
+B2_chlorosis_summary <- Batch_2 %>%
+  group_by(Treatment) %>%
+  summarise(Max = max(X5.dpi.Chlorosis)) %>%
+  left_join(B2_chlorosis_cld, by = c("Treatment" = "Group"))
+
+box_audps <- ggplot(Batch_2, aes(x = Treatment, y = X5.dpi.Chlorosis, fill = Treatment)) +
   geom_boxplot() +
+  geom_text(data = B2_chlorosis_summary,
+            aes(x = Treatment, y = Max + 0.5, label = Letter),  # adjust y offset as needed
+            size = 5, fontface = "bold") +
   theme_classic() +
-  labs(title="Col-0 Batch 2", subtitle = "Chlorosis Scores 5 d.p.i",  x ="Treatment", legend = "Treatment") +
-  labs(y="Disease Index Scores 0-10")
+  labs(title = "Col-0 Batch 2", subtitle = "Chlorosis Scores 5 d.p.i",
+       x = "Treatment", y = "Disease Index Scores 0-10")
 box_audps
 
 #Batch 7
 # Leaf Damage
-#import clds
 B7_leaf_damage_cld
-box_audps <- ggplot(Batch_7, aes(x=Treatment, y=PS..5.dpi.Leaf.Damage, fill=Treatment)) +
+B7_leaf_damage_summary <- Batch_7 %>%
+  group_by(Treatment) %>%
+  summarise(Max = max(PS..5.dpi.Leaf.Damage)) %>%
+  left_join(B7_leaf_damage_cld, by = c("Treatment" = "Group"))
+
+box_audps <- ggplot(Batch_7, aes(x = Treatment, y = PS..5.dpi.Leaf.Damage, fill = Treatment)) +
   geom_boxplot() +
+  geom_text(data = B7_leaf_damage_summary,
+            aes(x = Treatment, y = Max + 0.5, label = Letter),  # adjust y offset as needed
+            size = 5, fontface = "bold") +
   theme_classic() +
-  labs(title="Col-0 Batch 7", subtitle = "Leaf Damage Scores 5 d.p.i",  x ="Treatment", legend = "Treatment") +
-  labs(y="Disease Index Scores 0-10")
+  labs(title = "Col-0 Batch 7", subtitle = "Leaf Damage Scores 5 d.p.i",
+       x = "Treatment", y = "Disease Index Scores 0-10")
 box_audps
 
 # Chlorosis 
 B7_chlorosis_cld
-box_audps <- ggplot(Batch_7, aes(x=Treatment, y=PS..5.dpi.Chlorosis, fill=Treatment)) +
+B7_chlorosis_summary <- Batch_7 %>%
+  group_by(Treatment) %>%
+  summarise(Max = max(PS..5.dpi.Chlorosis)) %>%
+  left_join(B7_chlorosis_cld, by = c("Treatment" = "Group"))
+
+box_audps <- ggplot(Batch_7, aes(x = Treatment, y = PS..5.dpi.Chlorosis, fill = Treatment)) +
   geom_boxplot() +
+  geom_text(data = B7_chlorosis_summary,
+            aes(x = Treatment, y = Max + 0.5, label = Letter),  # adjust y offset as needed
+            size = 5, fontface = "bold") +
   theme_classic() +
-  labs(title="Col-0 Batch 7", subtitle = "Chlorosis Scores 7 d.p.i",  x ="Treatment", legend = "Treatment") +
-  labs(y="Disease Index Scores 0-10")
+  labs(title = "Col-0 Batch 7", subtitle = "Chlorosis Scores 5 d.p.i",
+       x = "Treatment", y = "Disease Index Scores 0-10")
+box_audps
 box_audps
