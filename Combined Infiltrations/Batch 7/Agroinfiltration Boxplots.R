ed18a668c2249fec7feeec8b1c1d644db69e5ad3
yourGitHubUsername <jjh62@kent.ac.uk>
2025-07-28 16:05:15 +0100

    Added new boxplots to the repository

--- Combined Infiltrations/Batch 7/Agroinfiltration Boxplots.R
+++ Combined Infiltrations/Batch 7/Agroinfiltration Boxplots.R
@@ -14,7 +14,7 @@ Batch_7 <- read.csv(file="/Users/joshhoti/Library/CloudStorage/OneDrive-Universi
                     header = TRUE, sep = ",", quote = "\"",
                     dec = ".", fill = TRUE, comment.char = "")
 Batch_7 <- na.omit(Batch_7)
-Batch_7 <- subset(Batch_7, Treatment != "Ctrl.HP.I")
+Batch_7$Treatment[Batch_7$Treatment == "Ctrl-HP-I"] <- "Ctrl.HP.I"
 Batch_7$Treatment[Batch_7$Treatment == "EHA105-I"] <- "EHA105.I"
 Batch_7$Treatment[Batch_7$Treatment == "Water-I"] <- "Water.I"
 Batch_7$Treatment[Batch_7$Treatment == "GFP-I"] <- "GFP.I"
@@ -26,10 +26,10 @@ Batch_7$Treatment[Batch_7$Treatment == "GFP-M"] <- "GFP.M"
 B7_leaf_damage_cld
 B7_leaf_damage_summary <- Batch_7 %>%
   group_by(Treatment) %>%
-  summarise(Max = max(PS..5.dpi.Leaf.Damage)) %>%
+  summarise(Max = max(Agro..5.dpi.Leaf.Damage)) %>%
   left_join(B7_leaf_damage_cld, by = c("Treatment" = "Group"))
 
-box_audps <- ggplot(Batch_7, aes(x = Treatment, y = PS..5.dpi.Leaf.Damage, fill = Treatment)) +
+box_audps <- ggplot(Batch_7, aes(x = Treatment, y = Agro..5.dpi.Leaf.Damage, fill = Treatment)) +
   geom_boxplot() +
   geom_text(data = B7_leaf_damage_summary,
             aes(x = Treatment, y = Max + 0.5, label = Letter),  # adjust y offset as needed
@@ -43,10 +43,10 @@ print(box_audps)
 B7_chlorosis_cld
 B7_chlorosis_summary <- Batch_7 %>%
   group_by(Treatment) %>%
-  summarise(Max = max(PS..5.dpi.Chlorosis)) %>%
+  summarise(Max = max(Agro..5.dpi.Chlorosis)) %>%
   left_join(B7_chlorosis_cld, by = c("Treatment" = "Group"))
 
-box_audps <- ggplot(Batch_7, aes(x = Treatment, y = PS..5.dpi.Chlorosis, fill = Treatment)) +
+box_audps <- ggplot(Batch_7, aes(x = Treatment, y = Agro..5.dpi.Chlorosis, fill = Treatment)) +
   geom_boxplot() +
   geom_text(data = B7_chlorosis_summary,
             aes(x = Treatment, y = Max + 0.5, label = Letter),  # adjust y offset as needed
